b3881639ed657f9b03b9d26814e1b9e7730cfe3f
magomadovmansur <95755736+magomadovmansur@users.noreply.github.com>
2021-12-16 16:26:54 +0300

    Update ping-pong.py
    this is 2 version ping-pong

--- ping-pong.py
+++ ping-pong.py
@@ -5,10 +5,9 @@ display.set_caption("ping-pong")
 game = True
 finish = False
 font.init()
-font1 = font.SysFont("Arial", 70)
-win = font1.render('YOU WIN', True, (215, 215, 0))
-lose = font1.render('YOU LOSE', True, (255, 0, 0))
-font2 = font.SysFont(None, 36)
+font1 = font.SysFont("Arial", 35)
+win = font1.render('PLAYER1 LOSE!', True, (215, 215, 0))
+lose = font1.render('PLAYER2 LOSE!', True, (255, 0, 0))
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
@@ -19,7 +18,22 @@ class GameSprite(sprite.Sprite):
         self.rect.y = player_y
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-class Player(GameSprite):
+class Wall(sprite.Sprite):
+    def __init__(self, color1, color2, color3, wall_x, wall_y, wall_widht, wall_height, speed):
+        super().__init__()
+        self.color1 = color1
+        self.color2 = color2
+        self.color3 = color3
+        self.widht = wall_widht
+        self.height = wall_height
+        self.image = Surface((self.widht, self.height))
+        self.image.fill((color1, color2, color3))
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+        self.speed = speed
+    def draw_wall(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 0:
@@ -32,8 +46,12 @@ class Player(GameSprite):
             self.rect.y = self.rect.y - self.speed
         if keys[K_DOWN] and self.rect.y < 320:
             self.rect.y = self.rect.y + self.speed
-player1 = Player("nchart-39304__340.png", 0, 300, 40, 180, 5)
-player2 = Player("nchart-39304__340.png", 660, 300, 40, 180, 5)
+player1 = Wall(28, 238, 150, 0, 300, 40, 180, 5)
+player2 = Wall(28, 238, 150, 660, 300, 40, 180, 5)
+x = 350
+y = 250
+speed_x = 6
+speed_y = 6
 time = time.Clock()
 FPS = 60
 while game:
@@ -42,9 +60,22 @@ while game:
             game = False
     if finish == False:
         window.fill(colour)
-        player1.reset()sssss
-        player2.reset()
+        player1.draw_wall()
+        player2.draw_wall()
         player1.update_l()
         player2.update_r()
+        draw.circle(window, (150, 150, 0), (x, y), 50)
+        x += speed_x
+        y += speed_y
+        if y >= 450 or y <= 50:
+            speed_y *= -1
+        if player1.rect.y <= y <= player1.rect.y + 230 and 40 >= x - 50 >= 0 or player2.rect.y <= y + 50 <= player2.rect.y + 230 and 660 <= x + 50 <= 700:
+            speed_x *= -1
+        if x < -50:
+            finish = True
+            window.blit(win, (200, 200))
+        if x > 700:
+            finish = True
+            window.blit(lose, (200, 200))
     display.update()
     time.tick(FPS)
